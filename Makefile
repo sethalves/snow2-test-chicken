04e745c48fafc298d42d8b6d51e6a7d44a082fba
Seth Alves <seth@gloebit.com>
2013-11-13 06:53:20 -0800

    track changes in chicken's r7rs egg

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 #
 
 snow2=../snow2-client/snow2-client-chicken.scm
+# snow2=snow2-client-chicken.scm
 
 
 all: base64
@@ -10,7 +11,7 @@ all: base64
 clean:
 	rm -f *~
 	$(snow2) uninstall '(seth base64)'
-	rm -rf base64 bytevector
+	rm -rf seth
 
 base64:
 	$(snow2) install '(seth base64)'
--- okay.scm
+++ okay.scm
@@ -1,8 +1,16 @@
-#!/usr/local/bin/csi -script
+#! /bin/sh
+#| -*- scheme -*-
+exec csi -s $0 "$@"
+|#
+
+;; #!/usr/local/bin/csi -script
+
+;; (import chicken scheme)
+(use r7rs)
 
 (include "seth/bytevector.sld")
-(import seth-bytevector)
+(import (seth bytevector))
 (include "seth/base64.sld")
-(import seth-base64)
+(import (seth base64))
 
-(format #t "~A\n" (base64-encode~ (string->utf8 "testing")))
+(format #t "~A\n" (encode (string->utf8 "testing")))
